84a500aa4b25f48a74844497e63ff99050d2dad8
Oleg Shabashov <oleg.shabashov@gmail.com>
2017-08-30 11:02:52 +0700

    Added CheckErr(err)

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-    "github.com/jzelinskie/geddit"
     "fmt"
+    "os"
+
     "github.com/bwmarrin/discordgo"
     "github.com/joho/godotenv"
-    "os"
+    "github.com/jzelinskie/geddit"
 )
 
 func main() {
     err := godotenv.Load()
-    if err != nil {
-        panic("Error loading .env file")
-    }
+    CheckErr(err)
 
     o, err := geddit.NewOAuthSession(
         os.Getenv("REDDIT_CLIENT_ID"),
@@ -20,15 +19,11 @@ func main() {
         "Testing OAuth Bot by u/my_user v0.1 see source https://github.com/jzelinskie/geddit",
         os.Getenv("REDDIT_REDIRECT_URL"),
     )
-    if err != nil {
-        fmt.Println(err)
-    }
+    CheckErr(err)
 
     // Create new auth token for confidential clients (personal scripts/apps).
     err = o.LoginAuth(os.Getenv("REDDIT_USERNAME"), os.Getenv("REDDIT_PASSWORD"))
-    if err != nil {
-        fmt.Println(err)
-    }
+    CheckErr(err)
 
     // Ready to make API calls!
     subOpts := geddit.ListingOptions{
@@ -39,16 +34,17 @@ func main() {
 
     // Connect to Discord
     dg, err := discordgo.New(os.Getenv("DISCORD_BOT_ID"))
-    if err != nil {
-        fmt.Println("Error creating Discord session: ", err)
-        return
-    }
+    CheckErr(err)
 
     // Print title and author of each submission
     for _, s := range submissions {
-        _, err := dg.ChannelMessageSend(os.Getenv("DISCORD_CHANNEL_ID"), fmt.Sprintf("%s\n%s\n",s.Title, s.URL))
-        if err != nil {
-            fmt.Println("Error posting: ", err)
-        }
+        _, err := dg.ChannelMessageSend(os.Getenv("DISCORD_CHANNEL_ID"), fmt.Sprintf("%s\n%s\n", s.Title, s.URL))
+        CheckErr(err)
+    }
+}
+
+func CheckErr(err error) {
+    if err != nil {
+        panic(err)
     }
 }
