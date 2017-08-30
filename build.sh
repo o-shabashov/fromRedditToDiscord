c4301e8c674921ba026fed53328027d530ae5b94
Oleg Shabashov <oleg.shabashov@gmail.com>
2017-08-30 10:54:30 +0700

    Updated output path to compiled binaries

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-env GOOS=windows GOARCH=amd64 go build -o bin/rtd.exe
-env GOOS=linux GOARCH=amd64 go build -o bin/rtd.sh
-env GOOS=darwin GOARCH=amd64 go build -o bin/rtd.mac
+env GOOS=windows GOARCH=amd64 go build -o rtd.exe
+env GOOS=linux GOARCH=amd64 go build -o rtd.sh
+env GOOS=darwin GOARCH=amd64 go build -o rtd.mac
